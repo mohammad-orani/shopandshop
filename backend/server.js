89d5ec1603ab2805cf4a29d0ff89e3b6acd10915
Mohammad Orani <mohammad-orani@example.com>
2026-03-03 01:46:28 +0300

    fix: cors for primejo.store

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(cors({
     origin: [
         'https://primejo.store',
         'https://www.primejo.store',
+        'https://adminprimejo.netlify.app',
         'http://127.0.0.1:5500',
         'http://localhost:5500',
         'http://localhost:3000'
