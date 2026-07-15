018d74674b16883804a39ef1e170cd064a92aeda
Mohammad Orani <mohammad-orani@example.com>
2026-07-15 14:07:08 +0300

    update direction

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,8 @@ app.use(cors({
     origin: [
         `https://${storeDomain}`,
         `https://www.${storeDomain}`,
-        'https://adminprimejo.netlify.app',
+        'https://adminshopandshop.netlify.app',
+        'https://clientshopandshop.netlify.app',
         'http://127.0.0.1:5500',
         'http://localhost:5500',
         'http://localhost:3000',
